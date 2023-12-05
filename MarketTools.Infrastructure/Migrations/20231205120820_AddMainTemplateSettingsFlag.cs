ebf481368f9d4654227b46bd4425e0ba25d8941d
Disaylt <disofban@gmail.com>
2023-12-05 15:08:43 +0300

    init: create migration

---
+++ MarketTools.Infrastructure/Migrations/20231205120820_AddMainTemplateSettingsFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketTools.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMainTemplateSettingsFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AsMainTemplate",
+                table: "AutoresponderTemplateSettings",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AsMainTemplate",
+                table: "AutoresponderTemplateSettings");
+        }
+    }
+}
