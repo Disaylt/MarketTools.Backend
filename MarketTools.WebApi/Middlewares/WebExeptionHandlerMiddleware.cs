cf4ace3b255daffcf84002ae9b92def00a80d301
Disaylt <disofban@gmail.com>
2023-12-07 00:06:11 +0300

    init: create ValidationExceptionHandlerService

---
+++ MarketTools.WebApi/Middlewares/WebExeptionHandlerMiddleware.cs
@@ -0,0 +1,20 @@
+﻿using MarketTools.Application.Interfaces.Identity;
+using System.Security.Claims;
+
+namespace MarketTools.WebApi.Middlewares
+{
+    public class WebExeptionHandlerMiddleware(RequestDelegate _next)
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ MarketTools.WebApi/Services/Exceptions/ValidationExceptionHandlerService.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using MarketTools.WebApi.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MarketTools.WebApi.Services.Exceptions
+{
+    public class ValidationExceptionHandlerService : IWebExceptionHandlerService<ValidationException>
+    {
+        public async Task HandleAsync(HttpContext context, ValidationException exception)
+        {
+            IEnumerable<string> errorMessagess = exception.Errors
+                .Select(x=> x.ErrorMessage)
+                .ToList();
+
+            ProblemDetails problemDetails = new ProblemDetails
+            {
+                Type = "MediatR validation exception",
+                Title = exception.Message,
+                Status = (int)HttpStatusCode.BadRequest,
+                Extensions = new Dictionary<string, object?>()
+                {
+                    {"ValidationErrors",  errorMessagess }
+                }
+            };
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            await context.Response.WriteAsJsonAsync(problemDetails);
+        }
+    }
+}
