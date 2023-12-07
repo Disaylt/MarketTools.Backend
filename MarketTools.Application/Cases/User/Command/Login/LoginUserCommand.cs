8b1c245c681da71454c808bfcf4a13b5499d0b94
Disaylt <disofban@gmail.com>
2023-12-07 11:59:34 +0300

    init: create login command

---
+++ MarketTools.Application/Cases/User/Command/Login/LoginUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using MarketTools.Application.Cases.User.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.User.Command.Login
+{
+    public class LoginUserCommand : IRequest<TokenVm>
+    {
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+    }
+}
