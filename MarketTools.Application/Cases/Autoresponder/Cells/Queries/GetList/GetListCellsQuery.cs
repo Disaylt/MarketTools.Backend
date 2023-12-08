3727da3115cce0b6d0d42c491cef014feb8c60b6
Disaylt <disofban@gmail.com>
2023-12-08 22:37:51 +0300

    init: create query for gett list cells

---
+++ MarketTools.Application/Cases/Autoresponder/Cells/Queries/GetList/GetListCellsQuery.cs
@@ -0,0 +1,15 @@
+﻿using MarketTools.Application.Cases.Autoresponder.Cells.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.Autoresponder.Cells.Queries.GetList
+{
+    public class GetListCellsQuery : IRequest<IEnumerable<CellVm>>
+    {
+        public int CollumnId { get; set; }
+    }
+}
---
+++ MarketTools.Application/Cases/Autoresponder/Cells/Queries/GetList/QueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MarketTools.Application.Cases.Autoresponder.Cells.Models;
+using MarketTools.Application.Interfaces.Database;
+using MarketTools.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.Autoresponder.Cells.Queries.GetList
+{
+    public class QueryHandler
+        (IAuthUnitOfWork _authUnitOfWork,
+        IMapper _mapper)
+        : IRequestHandler<GetListCellsQuery, IEnumerable<CellVm>>
+    {
+        public async Task<IEnumerable<CellVm>> Handle(GetListCellsQuery request, CancellationToken cancellationToken)
+        {
+            IEnumerable<AutoresponderCell> entities = await _authUnitOfWork.AutoresponderCells
+                .GetRangeAsync(x => x.ColumnId == request.CollumnId);
+
+            return _mapper.Map<IEnumerable<CellVm>>(entities);
+        }
+    }
+}
