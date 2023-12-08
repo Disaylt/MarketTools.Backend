dfee2c7fe4801b1bbb1343067625371a96da5ceb
Disaylt <disofban@gmail.com>
2023-12-08 19:21:25 +0300

    init: create command for update cell

---
+++ MarketTools.Application/Cases/Autoresponder/Cells/Commands/Update/CommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using MarketTools.Application.Cases.Autoresponder.Cells.Models;
+using MarketTools.Application.Common.Exceptions;
+using MarketTools.Application.Interfaces.Database;
+using MarketTools.Application.Interfaces.Identity;
+using MarketTools.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.Autoresponder.Cells.Commands.Update
+{
+    public class CommandHandler
+        (IMapper _mapper,
+        IMainDatabaseContext _context,
+        IAuthReadHelper _authReadHelper)
+        : IRequestHandler<UpdateCellCommand, CellVm>
+    {
+        public async Task<CellVm> Handle(UpdateCellCommand request, CancellationToken cancellationToken)
+        {
+            AutoresponderCell entity = await _context.AutoresponderCells
+                .FirstOrDefaultAsync(x => x.Id == request.Id && x.Column.UserId == _authReadHelper.UserId)
+                ?? throw new DefaultNotFoundException();
+
+            entity.Value = request.Value;
+
+            _context.AutoresponderCells.Update(entity);
+            await _context.SaveChangesAsync();
+
+            return _mapper.Map<CellVm>(entity);
+        }
+    }
+}
---
+++ MarketTools.Application/Cases/Autoresponder/Cells/Commands/Update/UpdateCellCommand.cs
@@ -0,0 +1,16 @@
+﻿using MarketTools.Application.Cases.Autoresponder.Cells.Models;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.Autoresponder.Cells.Commands.Update
+{
+    public class UpdateCellCommand : IRequest<CellVm>
+    {
+        public int Id { get; set; }
+        public required string Value { get; set; }
+    }
+}
