aacb6ce8e0e6adf0c31a3fe4cf8805ae0e56a38d
Disaylt <disofban@gmail.com>
2023-12-08 20:31:12 +0300

    init: create interfaces for IRepository and untiofwork

---
+++ MarketTools.Application/Interfaces/Database/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Interfaces.Database
+{
+    public interface IAuthRepository<T> : IRepository<T>  where T : class
+    {
+
+    }
+}
