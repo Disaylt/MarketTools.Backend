d5d5db4312c867cb1d36a536082bdd6e99b484cd
Disaylt <disofban@gmail.com>
2023-12-09 12:16:56 +0300

    init: create command for delete template

---
+++ MarketTools.Application/Cases/Autoresponder/Tempaltes/Commands/Delete/CommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Application.Models.Commands;
+using MarketTools.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.Autoresponder.Tempaltes.Commands.Delete
+{
+    public class CommandHandler
+        (IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<DefaultDeleteCommand<AutoresponderTemplate>>
+    {
+        public async Task Handle(DefaultDeleteCommand<AutoresponderTemplate> request, CancellationToken cancellationToken)
+        {
+            AutoresponderTemplate entity = await _authUnitOfWork.AutoresponderTemplates
+                .FirstAsync(x => x.Id == request.Id);
+            _authUnitOfWork.AutoresponderTemplates.Remove(entity);
+            await _authUnitOfWork.CommintAsync();
+        }
+    }
+}
