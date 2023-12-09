22a5da2465d00a372649ffcde05c4c149bcf0d25
Disaylt <disofban@gmail.com>
2023-12-09 18:43:27 +0300

    init: create articles controller with get and delete method

---
+++ MarketTools.WebApi/Controllers/Api/V1/Autoresponder/Template/ArticlesController.cs
@@ -0,0 +1,37 @@
+﻿using MarketTools.Application.Cases.Autoresponder.Tempaltes.Articles.Commands.DeleteAll;
+using MarketTools.Application.Cases.Autoresponder.Tempaltes.Articles.Models;
+using MarketTools.Application.Cases.Autoresponder.Tempaltes.Articles.Queries.GetList;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketTools.WebApi.Controllers.Api.V1.Autoresponder.Template
+{
+    [Route("api/v1/autoresponder/template/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class ArticlesController
+        (IMediator _mediator)
+        : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAsync(int templateId)
+        {
+            GetArticlesQuery query = new GetArticlesQuery { TemplateId = templateId };
+            IEnumerable<ArticleVm> result = await _mediator.Send(query);
+
+            return Ok(result);
+        }
+
+        [HttpDelete]
+        [Route("all")]
+        public async Task<IActionResult> DeleteAsync(int templateId)
+        {
+            DeleteAllArticlesCommand command = new DeleteAllArticlesCommand {  TemplateId = templateId };
+            await _mediator.Send(command);
+
+            return Ok();
+        }
+    }
+}
