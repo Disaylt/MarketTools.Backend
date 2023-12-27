1c124be1c293622a7379131a0a1b7e2d4da4795f
Disaylt <disofban@gmail.com>
2023-12-27 23:36:34 +0300

    init: create command validator for recommendation products

---
+++ MarketTools.Application/Cases/Autoresponder/Standard/RecommendationProducts/Commands/Create/CommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MarketTools.Application.Interfaces.Autoresponder.Standard;
+using MarketTools.Application.Interfaces.Autoresponder.Standard.Models;
+using MarketTools.Application.Interfaces.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.Autoresponder.Standard.RecommendationProducts.Commands.Create
+{
+    public class CommandValidator : AbstractValidator<CreateCommand>
+    {
+        public CommandValidator(IAuthUnitOfWork authUnitOfWork, IStandardAutoresponderLimitationsService standardAutoresponderLimitationsService)
+        {
+            RuleFor(x => x)
+                .MustAsync(async (recommendationProduct, ct) =>
+                {
+                    StandardAutoresponderLimitsDto limits = await standardAutoresponderLimitationsService.GetAsync();
+                    int totalRecommendationProducts = await authUnitOfWork.StandardAutoresponderRecommendationProducts.CountAsync();
+
+                    return totalRecommendationProducts < limits.MaxRecommendationProducts;
+                })
+                .WithErrorCode("400")
+                .WithMessage("Превышен лимит шаблонов.");
+        }
+    }
+}
