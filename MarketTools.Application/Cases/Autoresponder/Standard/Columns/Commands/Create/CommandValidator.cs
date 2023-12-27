caf4e80ea3f8858c6bd75b49b3c0676f8e93325d
Disaylt <disofban@gmail.com>
2023-12-27 23:12:31 +0300

    init: create command validator for columns

---
+++ MarketTools.Application/Cases/Autoresponder/Standard/Columns/Commands/Create/CommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MarketTools.Application.Interfaces.Autoresponder.Standard;
+using MarketTools.Application.Interfaces.Autoresponder.Standard.Models;
+using MarketTools.Application.Interfaces.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Cases.Autoresponder.Standard.Columns.Commands.Create
+{
+    public class CommandValidator : AbstractValidator<CreateCommand>
+    {
+        public CommandValidator(IAuthUnitOfWork authUnitOfWork, IStandardAutoresponderLimitationsService standardAutoresponderLimitationsService)
+        {
+            RuleFor(x => x)
+                .MustAsync(async (columnId, ct) =>
+                {
+                    StandardAutoresponderLimitsDto limits = await standardAutoresponderLimitationsService.GetAsync();
+                    int totalColumns = await authUnitOfWork.StandardAutoresponderColumns.CountAsync();
+
+                    return totalColumns < limits.MaxColumns;
+                })
+                .WithErrorCode("400")
+                .WithMessage("Превышен лимит колонок.");
+        }
+    }
+}
