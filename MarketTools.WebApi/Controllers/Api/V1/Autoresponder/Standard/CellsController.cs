5a84f30b6e430434d3a3deff08b16d1dfceac5b8
Disaylt <disofban@gmail.com>
2024-01-06 06:09:48 +0300

    init: create cells vm

--- MarketTools.WebApi/Controllers/Api/V1/Autoresponder/Standard/CellsController.cs
+++ MarketTools.WebApi/Controllers/Api/V1/Autoresponder/Standard/CellsController.cs
@@ -1,5 +1,7 @@
-﻿using MarketTools.Application.Cases.Autoresponder.Standard.Cells.Models;
+﻿using AutoMapper;
 using MarketTools.Application.Cases.Autoresponder.Standard.Cells.Queries.GetRange;
+using MarketTools.Domain.Entities;
+using MarketTools.WebApi.Models.Api.Autoreponder;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -10,15 +12,17 @@ namespace MarketTools.WebApi.Controllers.Api.V1.Autoresponder.Standard
     [Route("api/v1/autoresponder/standard/[controller]")]
     [ApiController]
     [Authorize]
-    public class CellsController(IMediator _mediator) : ControllerBase
+    public class CellsController(IMediator _mediator, IMapper _mapper) : ControllerBase
     {
         [HttpGet]
         public async Task<IActionResult> GetAsync(int columnId, CancellationToken cancellationToken)
         {
             GetRangeQuery query = new GetRangeQuery { CollumnId = columnId };
-            IEnumerable<CellVm> cells = await _mediator.Send(query, cancellationToken);
+            IEnumerable<StandardAutoresponderCell> entites = await _mediator.Send(query, cancellationToken);
 
-            return Ok(cells);
+            IEnumerable<CellVm> result = _mapper.Map<IEnumerable<CellVm>>(entites);
+
+            return Ok(result);
         }
     }
 }
