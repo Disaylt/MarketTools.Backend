ac8275728c074b990c98ed3b84a0dd71242bc20e
Disaylt <disofban@gmail.com>
2024-01-07 14:29:58 +0300

    init: create get range query handler

---
+++ MarketTools.Application/Requests/Autoresponder/Standard/BlackList/Queries/GetRange/GetRangeQuery.cs
@@ -0,0 +1,16 @@
+﻿using MarketTools.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.BlackList.Queries.GetRange
+{
+    public class GetRangeQuery
+        : IRequest<IEnumerable<StandardAutoresponderBlackListEntity>>
+    {
+        
+    }
+}
---
+++ MarketTools.Application/Requests/Autoresponder/Standard/BlackList/Queries/GetRange/QueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.BlackList.Queries.GetRange
+{
+    public class QueryHandler(IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<GetRangeQuery, IEnumerable<StandardAutoresponderBlackListEntity>>
+    {
+        private readonly IRepository<StandardAutoresponderBlackListEntity> _repository = _authUnitOfWork.StandardAutoresponderBlackLists;
+        public async Task<IEnumerable<StandardAutoresponderBlackListEntity>> Handle(GetRangeQuery request, CancellationToken cancellationToken)
+        {
+            return await _repository.GetRangeAsync(cancellationToken);
+        }
+    }
+}
