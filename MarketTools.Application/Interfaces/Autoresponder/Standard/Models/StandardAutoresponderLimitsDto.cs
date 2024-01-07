bf1eaa4b38883e1ec9066abe1f524eea4997aaa1
Disaylt <disofban@gmail.com>
2024-01-07 17:57:17 +0300

    init: create validator for black list add command

--- MarketTools.Application/Interfaces/Autoresponder/Standard/Models/StandardAutoresponderLimitsDto.cs
+++ MarketTools.Application/Interfaces/Autoresponder/Standard/Models/StandardAutoresponderLimitsDto.cs
@@ -13,8 +13,8 @@ namespace MarketTools.Application.Interfaces.Autoresponder.Standard.Models
         public int MaxBlackList { get; set; } = 10;
         public int MaxCells { get; set; } = 100;
         public int MaxColumns { get; set; } = 100;
-        public int MaxRecommendationProducts { get; set; } = 1000;
+        public int MaxRecommendationProducts { get; set; } = 10000;
         public int MaxTemplates { get; set; } = 100;
-        public int MaxTemplateArticles { get; set; } = 1000;
+        public int MaxTemplateArticles { get; set; } = 3000;
     }
 }
---
+++ MarketTools.Application/Requests/Autoresponder/Standard/BlackList/Commands/Add/CommandValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MarketTools.Application.Interfaces;
+using MarketTools.Application.Interfaces.Database;
+using MarketTools.Domain.Interfaces.Limits;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.BlackList.Commands.Add
+{
+    public class CommandValidator : AbstractValidator<BlackListAddCommand>
+    {
+        public CommandValidator(ILimitsService<IStandarAutoresponderLimits> limitsService, IAuthUnitOfWork authUnitOfWork) 
+        {
+            RuleFor(x => x)
+                .MustAsync(async (x, ct) =>
+                {
+                    IStandarAutoresponderLimits limits = await limitsService.GetAsync();
+                    int totalBlackLists = await authUnitOfWork.StandardAutoresponderBlackLists.CountAsync(ct);
+
+                    return totalBlackLists < limits.MaxBlackList;
+                })
+                .WithErrorCode("400")
+                .WithMessage("Превышен лимит черных списков.");
+        }
+    }
+}
