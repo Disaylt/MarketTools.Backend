6bf2a8bb3ed0d6123e2459dc1162693dea449f12
Disaylt <disofban@gmail.com>
2024-01-07 14:47:51 +0300

    init: create delete black list command handler

---
+++ MarketTools.Application/Requests/Autoresponder/Standard/BlackList/Commands/Delete/CommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Application.Models.Commands;
+using MarketTools.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.BlackList.Commands.Delete
+{
+    public class CommandHandler(IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<DefaultDeleteCommand<StandardAutoresponderBlackListEntity>>
+    {
+        private readonly IRepository<StandardAutoresponderBlackListEntity> _repository = _authUnitOfWork.StandardAutoresponderBlackLists;
+
+        public async Task Handle(DefaultDeleteCommand<StandardAutoresponderBlackListEntity> request, CancellationToken cancellationToken)
+        {
+            StandardAutoresponderBlackListEntity entity = await _repository.FirstAsync(x=> x.Id == request.Id);
+            _repository.Remove(entity);
+
+            await _authUnitOfWork.CommintAsync(cancellationToken);
+        }
+    }
+}
