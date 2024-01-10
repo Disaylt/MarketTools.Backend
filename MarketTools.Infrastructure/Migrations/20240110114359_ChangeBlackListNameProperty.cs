0e9024ce57af3bb018fc6834077caa20630b1829
Disaylt <disofban@gmail.com>
2024-01-10 14:44:24 +0300

    init: add migration

---
+++ MarketTools.Infrastructure/Migrations/20240110114359_ChangeBlackListNameProperty.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketTools.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeBlackListNameProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BindAutoresponerBlackListId",
+                table: "StandardAutoresponderTemplates",
+                newName: "BlackListId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StandardAutoresponderTemplates_BindAutoresponerBlackListId",
+                table: "StandardAutoresponderTemplates",
+                newName: "IX_StandardAutoresponderTemplates_BlackListId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BlackListId",
+                table: "StandardAutoresponderTemplates",
+                newName: "BindAutoresponerBlackListId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StandardAutoresponderTemplates_BlackListId",
+                table: "StandardAutoresponderTemplates",
+                newName: "IX_StandardAutoresponderTemplates_BindAutoresponerBlackListId");
+        }
+    }
+}
