9b0f4a86605046fd8718722fb70a8752a7ca862c
Disaylt <disofban@gmail.com>
2024-01-12 10:47:22 +0300

    init: add migration

---
+++ MarketTools.Infrastructure/Migrations/20240112074331_RenameArticleTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketTools.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameArticleTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Article",
+                table: "StandardAutoresponderTemplateArticles",
+                newName: "Value");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Value",
+                table: "StandardAutoresponderTemplateArticles",
+                newName: "Article");
+        }
+    }
+}
