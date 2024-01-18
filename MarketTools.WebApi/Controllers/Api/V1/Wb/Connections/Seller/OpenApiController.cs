25fbf73b54dbd760e8d21d4c132d6a67cd42ea1e
Disaylt <disofban@gmail.com>
2024-01-18 22:41:40 +0300

    init: create open api controller for wb with create method

---
+++ MarketTools.WebApi/Controllers/Api/V1/Wb/Connections/Seller/OpenApiController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MarketTools.Application.Requests.Wb.Connections.Seller.OpenApi.Commands.Add;
+using MarketTools.Domain.Entities;
+using MarketTools.WebApi.Models.Api.MarketplaceConnections;
+using MarketTools.WebApi.Models.Api.WB.Connections.Seller;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarketTools.WebApi.Controllers.Api.V1.Wb.Connections.Seller
+{
+    [Route("api/v1/wb/connections/seller/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class OpenApiController(IMediator _mediator, IMapper _mapper)
+        : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([FromBody] NewOpenApiBody body)
+        {
+            SellerOpenApiAddCommand command = _mapper.Map<SellerOpenApiAddCommand>(body);
+
+            MarketplaceConnectionEntity entitiy = await _mediator.Send(command);
+
+            MarketplaceConnectionVm viewEntity = _mapper.Map<MarketplaceConnectionVm>(entitiy);
+
+            return Ok(viewEntity);
+        }
+    }
+}
---
+++ MarketTools.WebApi/Models/Api/WB/Connections/Seller/NewOpenApiBody.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using MarketTools.Application.Common.Mappings;
+using MarketTools.Application.Requests.Wb.Connections.Seller.OpenApi.Commands.Add;
+using System.ComponentModel.DataAnnotations;
+
+namespace MarketTools.WebApi.Models.Api.WB.Connections.Seller
+{
+    public class NewOpenApiBody : IHasMap
+    {
+        [MaxLength(100, ErrorMessage = "Длинна названия не может превышать 100 символов.")]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(300, ErrorMessage = "Длинна описания не может превышать 300 символов.")]
+        public string? Description { get; set; }
+
+        [MaxLength(1000, ErrorMessage = "Длинна токена не может превышать 1000 символов.")]
+        public string Token { get; set; } = string.Empty;
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<NewOpenApiBody, SellerOpenApiAddCommand>();
+        }
+    }
+}
