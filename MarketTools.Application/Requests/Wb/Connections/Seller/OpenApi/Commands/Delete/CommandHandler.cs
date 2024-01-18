df57befa890ae3b9dd965b031a34f4150c0a6c02
Disaylt <disofban@gmail.com>
2024-01-18 21:21:44 +0300

    init: create command for delete wb connection

---
+++ MarketTools.Application/Requests/Wb/Connections/Seller/OpenApi/Commands/Delete/CommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Application.Models.Commands;
+using MarketTools.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Wb.Connections.Seller.OpenApi.Commands.Delete
+{
+    public class CommandHandler(IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<DefaultDeleteCommand<WbSellerOpenApiConnectionEntity>>
+    {
+
+        private readonly IRepository<WbSellerOpenApiConnectionEntity> _repository = _authUnitOfWork.WbSellerOpenApiConnections;
+
+        public async Task Handle(DefaultDeleteCommand<WbSellerOpenApiConnectionEntity> request, CancellationToken cancellationToken)
+        {
+            WbSellerOpenApiConnectionEntity entity = await _repository.FirstAsync(x=> x.Id == request.Id, cancellationToken);
+
+            _repository.Remove(entity);
+
+            await _authUnitOfWork.CommintAsync(cancellationToken);
+        }
+    }
+}
