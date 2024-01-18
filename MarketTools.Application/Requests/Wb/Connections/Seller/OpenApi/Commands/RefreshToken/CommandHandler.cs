1b5dd55a9e615f319a4e8af5e87ca17d02d1ddae
Disaylt <disofban@gmail.com>
2024-01-18 20:44:35 +0300

    init: create refresh token command

---
+++ MarketTools.Application/Requests/Wb/Connections/Seller/OpenApi/Commands/RefreshToken/CommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Wb.Connections.Seller.OpenApi.Commands.RefreshToken
+{
+    public class CommandHandler(IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<OpenApiRefreshTokenCommand>
+    {
+
+        private readonly IRepository<WbSellerOpenApiConnectionEntity> _repository = _authUnitOfWork.WbSellerOpenApiConnections;
+
+        public async Task Handle(OpenApiRefreshTokenCommand request, CancellationToken cancellationToken)
+        {
+            WbSellerOpenApiConnectionEntity entity = await _repository.FirstAsync(x => x.Id == request.Id);
+
+            entity.Token = request.Token;
+            entity.IsActive = true;
+
+            _repository.Update(entity);
+            await _authUnitOfWork.CommintAsync(cancellationToken);
+        }
+    }
+}
---
+++ MarketTools.Application/Requests/Wb/Connections/Seller/OpenApi/Commands/RefreshToken/OpenApiRefreshTokenCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Wb.Connections.Seller.OpenApi.Commands.RefreshToken
+{
+    public class OpenApiRefreshTokenCommand : IRequest
+    {
+        public int Id { get; set; }
+        public required string Token { get; set; }
+    }
+}
