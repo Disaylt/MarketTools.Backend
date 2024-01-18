7ca1b2c660dba87347aa6a5068a21e297137b117
Disaylt <disofban@gmail.com>
2024-01-18 20:58:44 +0300

    init: create get range query

---
+++ MarketTools.Application/Models/Queries/GetRangeQuery.cs
@@ -0,0 +1,15 @@
+﻿using MarketTools.Domain.Common;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Models.Queries
+{
+    public class GetRangeQuery<T> : PageRequest, IRequest<IEnumerable<T>>
+    {
+
+    }
+}
---
+++ MarketTools.Application/Requests/Wb/Connections/Seller/OpenApi/Queries/GetRange/QueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Application.Models.Queries;
+using MarketTools.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Wb.Connections.Seller.OpenApi.Queries.GetRange
+{
+    public class QueryHandler(IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<GetRangeQuery<WbSellerOpenApiConnectionEntity>, IEnumerable<WbSellerOpenApiConnectionEntity>>
+    {
+        public async Task<IEnumerable<WbSellerOpenApiConnectionEntity>> Handle(GetRangeQuery<WbSellerOpenApiConnectionEntity> request, CancellationToken cancellationToken)
+        {
+            return await _authUnitOfWork.WbSellerOpenApiConnections
+                .GetAsQueryable()
+                .Skip(request.Skip)
+                .Take(request.Take)
+                .ToListAsync();
+        }
+    }
+}
