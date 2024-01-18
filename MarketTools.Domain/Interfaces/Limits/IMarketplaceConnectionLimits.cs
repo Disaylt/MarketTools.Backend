5cadf36134bfa078da6b923ef233a8c58799f6bf
Disaylt <disofban@gmail.com>
2024-01-18 20:21:13 +0300

    init: create connection limits interface

---
+++ MarketTools.Domain/Interfaces/Limits/IMarketplaceConnectionLimits.cs
@@ -0,0 +1,14 @@
+﻿using MarketTools.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Domain.Interfaces.Limits
+{
+    public interface IMarketplaceConnectionLimits : ILimitModel
+    {
+        public int MaxConnections { get; set; }
+    }
+}
