62371c82b709cf6e7bb1b09721d79333beaf0192
Disaylt <disofban@gmail.com>
2024-01-19 12:19:30 +0300

    init: create query handler for marketplace connections

--- MarketTools.Application/Requests/MarketplaceConnections/Queries/GetRangePagination/QueryHandler.cs
+++ MarketTools.Application/Requests/MarketplaceConnections/Queries/GetRangePagination/QueryHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Application.Utilities.MarketplaceConnections;
+using MarketTools.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +11,19 @@ using System.Threading.Tasks;
 
 namespace MarketTools.Application.Requests.MarketplaceConnections.Queries.GetRangePagination
 {
-    public class QueryHandler
+    public class QueryHandler(IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<GetRangePaginationMarketplaceConnectionsQuery, IEnumerable<MarketplaceConnectionEntity>>
     {
+        public async Task<IEnumerable<MarketplaceConnectionEntity>> Handle(GetRangePaginationMarketplaceConnectionsQuery request, CancellationToken cancellationToken)
+        {
+            string discriminator = new MarketplaceConnectionsDiscriminatorFactory().Get(request.ConnectionType);
 
+            return await _authUnitOfWork.SellerConnections
+                .GetAsQueryable()
+                .Where(x => x.Description == discriminator)
+                .Skip(request.Skip)
+                .Take(request.Take)
+                .ToListAsync();
+        }
     }
 }
