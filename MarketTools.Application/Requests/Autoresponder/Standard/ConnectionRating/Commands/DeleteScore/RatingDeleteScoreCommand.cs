f8df2e0924ffad9cce86ff6254e80e10051f831e
Disaylt <disofban@gmail.com>
2024-01-22 23:53:56 +0300

    init: command handler

---
+++ MarketTools.Application/Requests/Autoresponder/Standard/ConnectionRating/Commands/DeleteScore/RatingDeleteScoreCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.ConnectionRating.Commands.DeleteScore
+{
+    public class RatingDeleteScoreCommand : IRequest
+    {
+        public int Rating { get; set; }
+        public int ConnectionId { get; set; }
+    }
+}
