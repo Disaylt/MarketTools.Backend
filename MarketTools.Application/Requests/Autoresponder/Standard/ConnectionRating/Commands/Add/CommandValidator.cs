021dac5c48d046aadae7ddc2548420d80c38929b
Disaylt <disofban@gmail.com>
2024-01-23 13:19:28 +0300

    init: create command validator

---
+++ MarketTools.Application/Requests/Autoresponder/Standard/ConnectionRating/Commands/Add/CommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using MarketTools.Application.Interfaces.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.ConnectionRating.Commands.Add
+{
+    public class CommandValidator : AbstractValidator<AddRatingCommand>
+    {
+        public CommandValidator(IAuthUnitOfWork authUnitOfWork) 
+        {
+            RuleFor(x => x.ConnectionId)
+                .MustAsync(async (connectionId, ct) =>
+                {
+                    return await authUnitOfWork.SellerConnections
+                        .AnyAsync(x => x.Id == connectionId, ct);
+                })
+                .WithErrorCode("404")
+                .WithMessage("Подключение не найдено.");
+        }
+    }
+}
