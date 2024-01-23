1f4f8a80b392fadd7c78e0b5da9754deb628e481
Disaylt <disofban@gmail.com>
2024-01-23 15:14:46 +0300

    init: create ratings controller

--- MarketTools.WebApi/Controllers/Api/V1/Autoresponder/Standard/Connection/RatingsController.cs
+++ MarketTools.WebApi/Controllers/Api/V1/Autoresponder/Standard/Connection/RatingsController.cs
@@ -1,14 +1,13 @@
 ﻿using AutoMapper;
-using MarketTools.Application.Requests.Autoresponder.Standard.ConnectionRating.Commands.Add;
-using MarketTools.Application.Requests.Autoresponder.Standard.ConnectionRating.Commands.AddTemplate;
-using MarketTools.Application.Requests.Autoresponder.Standard.ConnectionRating.Commands.DeleteScore;
-using MarketTools.Application.Requests.Autoresponder.Standard.ConnectionRating.Commands.DeleteTemplate;
+using MarketTools.Application.Requests.Autoresponder.Standard.ConnectionRating.Queries.GetRange;
 using MarketTools.Domain.Entities;
 using MarketTools.WebApi.Models.Api.Autoreponder.Standard;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace MarketTools.WebApi.Controllers.Api.V1.Autoresponder.Standard.Connection
 {
@@ -18,65 +17,18 @@ namespace MarketTools.WebApi.Controllers.Api.V1.Autoresponder.Standard.Connectio
     public class RatingsController(IMediator _mediator, IMapper _mapper)
         : ControllerBase
     {
-        [HttpPost]
-        public async Task<IActionResult> AddAsync(int rating, int connectionId)
+
+        [HttpGet]
+        public async Task<IActionResult> GetRangeAsync(int connectionId)
         {
-            AddRatingCommand command = new AddRatingCommand
+            GetRangeRatingsQuery query = new GetRangeRatingsQuery
             {
-                ConnectionId = connectionId,
-                Rating = rating
+                ConnectionId = connectionId
             };
+            IEnumerable<StandardAutoresponderConnectionRatingEntity> ratings = await _mediator.Send(query);
+            IEnumerable<RatingVm> viewRatings = _mapper.Map<IEnumerable<RatingVm>>(ratings);
 
-            await _mediator.Send(command);
-
-            return Ok();
-        }
-
-        [HttpPost]
-        [Route("template")]
-        public async Task<IActionResult> AddTemplateAsync(int rating, int connectionId, int templateId)
-        {
-            AddTemplateToRatingCommand command = new AddTemplateToRatingCommand
-            {
-                ConnectionId = connectionId,
-                Rating = rating,
-                TemplateId = templateId
-            };
-
-            StandardAutoresponderTemplateEntity template = await _mediator.Send(command);
-            TemplateVm viewTemplate = _mapper.Map<TemplateVm>(template);
-
-            return Ok(viewTemplate);
-        }
-
-        [HttpDelete]
-        public async Task<IActionResult> DeleteAsync(int rating, int connectionId)
-        {
-            RatingDeleteScoreCommand command = new RatingDeleteScoreCommand
-            {
-                ConnectionId = connectionId,
-                Rating = rating
-            };
-
-            await _mediator.Send(command);
-
-            return Ok();
-        }
-
-        [HttpDelete]
-        [Route("template")]
-        public async Task<IActionResult> DeleteTemplateAsync(int rating, int connectionId, int templateId)
-        {
-            RatingDeleteTemplateCommand command = new RatingDeleteTemplateCommand
-            {
-                ConnectionId = connectionId,
-                Rating = rating,
-                TemplateId = templateId
-            };
-
-            await _mediator.Send(command);
-
-            return Ok();
+            return Ok(viewRatings);
         }
     }
 }
