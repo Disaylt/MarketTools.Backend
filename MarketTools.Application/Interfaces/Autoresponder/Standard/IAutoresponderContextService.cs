be74a5c18bc32824af987a2090a80dc832dd18ce
Disaylt <disofban@gmail.com>
2024-01-26 17:37:26 +0300

    init: create autoresponderContextService

---
+++ MarketTools.Application/Interfaces/Autoresponder/Standard/IAutoresponderContextService.cs
@@ -0,0 +1,15 @@
+﻿using MarketTools.Application.Models.Autoresponder.Standard;
+using MarketTools.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Interfaces.Autoresponder.Standard
+{
+    public interface IAutoresponderContextService<T> where T : MarketplaceConnectionEntity
+    {
+        public AutoresponderContext GetContext(int connectionId);
+    }
+}
---
+++ MarketTools.Application/Models/Autoresponder/Standard/AutoresponderContext.cs
@@ -0,0 +1,15 @@
+﻿using MarketTools.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Models.Autoresponder.Standard
+{
+    public class AutoresponderContext
+    {
+        public IEnumerable<StandardAutoresponderRecommendationProductEntity> RecommendationProducts { get; set; } 
+            = Enumerable.Empty<StandardAutoresponderRecommendationProductEntity>(); 
+    }
+}
