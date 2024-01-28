d509914639c514f6cd519de688f13b3de4c2f534
Disaylt <disofban@gmail.com>
2024-01-29 02:36:03 +0300

    init: create migration

--- MarketTools.Infrastructure/Migrations/20240128233303_AddMarketplaceNameForBaseConnection.Designer.cs
+++ MarketTools.Infrastructure/Migrations/20240128233303_AddMarketplaceNameForBaseConnection.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using MarketTools.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MarketTools.Infrastructure.Migrations
 {
     [DbContext(typeof(MainAppDbContext))]
-    partial class MainAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240128233303_AddMarketplaceNameForBaseConnection")]
+    partial class AddMarketplaceNameForBaseConnection
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
