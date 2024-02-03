68e33f3b08bf045a95597c2def868ccdceadeffa
Disaylt <disofban@gmail.com>
2024-02-03 08:28:11 +0300

    feat: implement feedbacks http sender

---
+++ MarketTools.Infrastructure/Http/BaseHttpService.cs
@@ -0,0 +1,20 @@
+﻿using MarketTools.Application.Common.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Infrastructure.Http
+{
+    internal abstract class BaseHttpService
+    {
+        protected async Task<T> GetViewResultAsJson<T>(HttpResponseMessage httpResponseMessage)
+        {
+            return await httpResponseMessage.Content
+                .ReadFromJsonAsync<T>()
+                ?? throw new AppBadRequestException("Не удалось преобразовать ответ http запроса.");
+        }
+    }
+}
--- MarketTools.Infrastructure/Http/Wb/Seller/Api/FeedbacksHttpService.cs
+++ MarketTools.Infrastructure/Http/Wb/Seller/Api/FeedbacksHttpService.cs
@@ -1,15 +1,17 @@
-﻿using MarketTools.Application.Interfaces.Http.Wb.Seller.Api;
+﻿using MarketTools.Application.Common.Exceptions;
+using MarketTools.Application.Interfaces.Http.Wb.Seller.Api;
 using MarketTools.Domain.Http.WB.Seller.Api;
 using MarketTools.Domain.Http.WB.Seller.Api.Feedbaks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MarketTools.Infrastructure.Http.Wb.Seller.Api
 {
-    internal class FeedbacksHttpService : IFeedbacksHttpService
+    internal class FeedbacksHttpService : BaseHttpService, IFeedbacksHttpService
     {
         private readonly HttpClient _httpClient; 
 
@@ -19,14 +21,58 @@ namespace MarketTools.Infrastructure.Http.Wb.Seller.Api
             _httpClient.BaseAddress = new Uri("https://feedbacks-api.wildberries.ru");
         }
 
-        public Task<WbApiResult<FeedbackResponseData>> GetFeedbacksAsync(FeedbacksQuery query)
+        public async Task<WbApiResult<FeedbackResponseData>> GetFeedbacksAsync(FeedbacksQuery query)
         {
-            throw new NotImplementedException();
+            string path = CreatePathForGettingFeedbacks(query);
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, path);
+
+            HttpResponseMessage response = await _httpClient.SendAsync(requestMessage);
+
+            return await GetViewResultAsJson<WbApiResult<FeedbackResponseData>>(response);
         }
 
-        public Task SendResponseAsync(SendResponseBody body)
+        public async Task SendResponseAsync(SendResponseBody body)
         {
-            throw new NotImplementedException();
+            string path = "";
+            HttpContent content = JsonContent.Create(body);
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, path);
+            request.Content = content;
+
+            await _httpClient.SendAsync(request);
+        }
+
+        private string CreatePathForGettingFeedbacks(FeedbacksQuery query)
+        {
+            StringBuilder sb = new StringBuilder()
+                .Append("api/v1/feedbacks?");
+
+            List<string> requestParams = new List<string>
+            {
+                { $"isAnswered={query.IsAnswered}" },
+                { $"take={query.Take}" },
+                { $"skip={query.Skip}" },
+                { $"order={query.Order}" }
+            };
+            
+            if(query.NmId != null)
+            {
+                requestParams.Add($"nmId={query.NmId.Value}");
+            }
+
+            if(query.DateFrom != null)
+            {
+                requestParams.Add($"dateFrom={query.DateFrom.Value}");
+            }
+
+            if (query.DateTo != null)
+            {
+                requestParams.Add($"dateTo={query.DateTo.Value}");
+            }
+
+            sb.AppendJoin('&', requestParams);
+
+            return sb.ToString();
         }
     }
 }
