6393ad5d313d7f26c09e957c863157fe418926d7
Disaylt <disofban@gmail.com>
2024-02-06 07:27:50 +0300

    init: create worker service

---
+++ Presentations/WbStandardAutoresponder.WrokerService/Program.cs
@@ -0,0 +1,7 @@
+using WbStandardAutoresponder.WrokerService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
