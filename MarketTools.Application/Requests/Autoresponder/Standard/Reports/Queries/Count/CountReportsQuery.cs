fc2bb7bad7ad473ae3397d4105248473444c46f4
Disaylt <disofban@gmail.com>
2024-02-09 16:53:38 +0300

    init: create count reports query

---
+++ MarketTools.Application/Requests/Autoresponder/Standard/Reports/Queries/Count/CountReportsQuery.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.Reports.Queries.Count
+{
+    public class CountReportsQuery : IRequest<int>
+    {
+        public int? ConnectionId { get; set; }
+        public int? Rating { get; set; }
+        public bool? IsSuccess { get; set; }
+        public string? Article { get; set; }
+    }
+}
---
+++ MarketTools.Application/Requests/Autoresponder/Standard/Reports/Queries/Count/QueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MarketTools.Application.Interfaces.Database;
+using MarketTools.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Requests.Autoresponder.Standard.Reports.Queries.Count
+{
+    public class QueryHandler(IAuthUnitOfWork _authUnitOfWork)
+        : IRequestHandler<CountReportsQuery, int>
+    {
+        private readonly IRepository<StandardAutoresponderNotificationEntity> _repository = _authUnitOfWork.GetRepository<StandardAutoresponderNotificationEntity>();
+        public async Task<int> Handle(CountReportsQuery request, CancellationToken cancellationToken)
+        {
+            IQueryable<StandardAutoresponderNotificationEntity> query = _repository
+                .GetAsQueryable();
+
+            return await new ReportsQueryBuilder(query)
+                .SetConnectionId(request.ConnectionId)
+                .SetArticle(request.Article)
+                .SetRating(request.Rating)
+                .SetSuccesStatus(request.IsSuccess)
+                .Build()
+                .CountAsync();
+        }
+    }
+}
