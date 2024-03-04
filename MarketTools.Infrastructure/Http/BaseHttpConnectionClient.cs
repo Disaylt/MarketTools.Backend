984f19feecbfcc5c1cd9ced41518706ef587e3bf
Disaylt <disofban@gmail.com>
2024-03-04 11:06:14 +0300

    feat: add http conenction client options

--- MarketTools.Infrastructure/Http/BaseHttpConnectionClient.cs
+++ MarketTools.Infrastructure/Http/BaseHttpConnectionClient.cs
@@ -2,6 +2,7 @@
 using MarketTools.Application.Interfaces.Common;
 using MarketTools.Application.Interfaces.Http;
 using MarketTools.Domain.Entities;
+using MarketTools.Infrastructure.Http.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,18 +16,21 @@ namespace MarketTools.Infrastructure.Http
     {
         protected MarketplaceConnectionEntity Connection { get; }
         public HttpClient HttpClient { get; }
+        private readonly HttpConnectionClientOptions _options;
 
-        public BaseHttpConnectionClient(IContextService<MarketplaceConnectionEntity> connectionContextReader, HttpClient httpClient)
+        public BaseHttpConnectionClient(IContextService<MarketplaceConnectionEntity> connectionContextReader, HttpClient httpClient, HttpConnectionClientOptions? options = null)
         {
             Connection = connectionContextReader.Context;
             HttpClient = httpClient;
+
+            _options = options ?? new HttpConnectionClientOptions();
         }
 
         public virtual async Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage)
         {
             HttpResponseMessage httpResponseMessage = await HttpClient.SendAsync(httpRequestMessage);
 
-            if ((int)httpResponseMessage.StatusCode >= 400)
+            if ((int)httpResponseMessage.StatusCode >= 400 && _options.IsThrowBadRequestexception)
             {
                 throw new AppConnectionBadRequestException(Connection, httpResponseMessage.StatusCode);
             }
