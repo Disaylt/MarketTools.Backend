a4479764ca22e5c7476b092c83d6caa5ffbf90ac
Disaylt <disofban@gmail.com>
2024-03-11 17:39:15 +0300

    feat: delete old validator standard autoresponder

---
+++ MarketTools.Application/Interfaces/ProjectServices/IProjectServiceValidator.cs
@@ -0,0 +1,14 @@
+﻿using MarketTools.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarketTools.Application.Interfaces.ProjectServices
+{
+    public interface IProjectServiceValidator : IProjectService
+    {
+        public Task<bool> TryActivate(MarketplaceConnectionEntity connection);
+    }
+}
