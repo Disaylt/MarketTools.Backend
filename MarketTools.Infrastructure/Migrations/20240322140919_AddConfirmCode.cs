bc50816066c0887f16b5fb97e387a21a5cdac142
Disaylt <disofban@gmail.com>
2024-03-22 17:09:41 +0300

    init: create migration

---
+++ MarketTools.Infrastructure/Migrations/20240322140919_AddConfirmCode.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MarketTools.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConfirmCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmationCode",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ConfirmationCodeCreateDate",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfirmationCode",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmationCodeCreateDate",
+                table: "AspNetUsers");
+        }
+    }
+}
